ca43896b900eaf0dd654a2ce13c227d0d736bf08
cho881020 <cho881020@gmail.com>
2017-09-11 09:56:54 +0900

    텍스트파일 세이브 기능 작성

--- app/src/main/java/kr/co/tjeit/fileanddabase/util/TextFileManager.java
+++ app/src/main/java/kr/co/tjeit/fileanddabase/util/TextFileManager.java
@@ -2,6 +2,10 @@ package kr.co.tjeit.fileanddabase.util;
 
 import android.content.Context;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 /**
  * Created by user on 2017-09-11.
  */
@@ -14,6 +18,31 @@ public class TextFileManager {
 
     public TextFileManager(Context context) {
         mContext = context;
+    }
+
+    public void saveMemo(String inputData) {
+//        들어오는 스트링 객체를 -> 내장메모리의 파일로 저장.
+
+        if (inputData == null || inputData.equals("")) {
+//            메모의 내용이 들어오지 않으면 세이브를 취소.
+            return;
+        }
+
+        FileOutputStream fosMemo = null;
+
+        try {
+            fosMemo = mContext.openFileOutput(memoFileName, Context.MODE_PRIVATE);
+
+            fosMemo.write(inputData.getBytes());
+
+            fosMemo.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
