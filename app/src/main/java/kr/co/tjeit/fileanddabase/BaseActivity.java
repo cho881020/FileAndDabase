98c5724ba8b3842c48a10e8f276997ee2116cb3c
cho881020 <cho881020@gmail.com>
2017-09-11 09:44:32 +0900

    재 업로드

---
+++ app/src/main/java/kr/co/tjeit/fileanddabase/BaseActivity.java
@@ -0,0 +1,18 @@
+package kr.co.tjeit.fileanddabase;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by user on 2017-09-11.
+ */
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    Context mContext = this;
+
+    public abstract void setupEvents();
+    public abstract void setValues();
+    public abstract void bindViews();
+
+}
