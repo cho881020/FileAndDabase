214e8aedd5a3d14bc1cbacfeaec0c23b64231f5c
cho881020 <cho881020@gmail.com>
2017-09-11 12:04:33 +0900

    객체 저장 주석

--- app/src/main/java/kr/co/tjeit/fileanddabase/util/ObjectFileManager.java
+++ app/src/main/java/kr/co/tjeit/fileanddabase/util/ObjectFileManager.java
@@ -20,6 +20,8 @@ public class ObjectFileManager {
 //    Ex) .csv로 저장한다면 항목마다 사이사이에 쉼표를 끼워넣는다.
 //    다른 프로그램이 활용하지 않는 확장자를 독자적으로 사용한다면, 굳이 가공할 필요 없다.
     private static final String FILE_NAME = "memo.obj";
+
+//    파일의 입출력 경로를 불러오기 위한 도구
     Context mContext;
 
     public ObjectFileManager(Context context) {
@@ -27,16 +29,24 @@ public class ObjectFileManager {
     }
 
     public void save(HashMap<String, String> objData) {
+//        파일입출력을 담당하는 FOS.
         FileOutputStream fos = null;
+//        FOS를 통해 파일에 객체를 저장해주는 OOS.
         ObjectOutputStream oos = null;
 
         try {
+//            실제로 저장될 파일을 열기.
             fos = mContext.openFileOutput(FILE_NAME, Context.MODE_PRIVATE);
 
+//            열린파일 (fos)을 통해 데이터를 전해주는 ObjectOutputStream 생성.
             oos = new ObjectOutputStream(fos);
+
+//            oos를 통해 해쉬맵 데이터를 전달
             oos.writeObject(objData);
 
+//            늦게 연것을 먼저 닫기.
             oos.close();
+//            먼저 연것을 마지막에 닫음.
             fos.close();
 
 
