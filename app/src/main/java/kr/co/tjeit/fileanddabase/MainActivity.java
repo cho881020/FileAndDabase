c96db682eb96ef657433926b8529d6cc08592b50
cho881020 <cho881020@gmail.com>
2017-09-11 09:52:48 +0900

    텍스트파일 매니저 생성

--- app/src/main/java/kr/co/tjeit/fileanddabase/MainActivity.java
+++ app/src/main/java/kr/co/tjeit/fileanddabase/MainActivity.java
@@ -1,9 +1,15 @@
 package kr.co.tjeit.fileanddabase;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends BaseActivity {
+
+    private android.widget.Button loadBtn;
+    private android.widget.Button saveBtn;
+    private android.widget.Button deleteBtn;
+    private android.widget.EditText contentEdt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +32,10 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void bindViews() {
+        this.contentEdt = (EditText) findViewById(R.id.contentEdt);
+        this.deleteBtn = (Button) findViewById(R.id.deleteBtn);
+        this.saveBtn = (Button) findViewById(R.id.saveBtn);
+        this.loadBtn = (Button) findViewById(R.id.loadBtn);
 
     }
 }
---
+++ app/src/main/java/kr/co/tjeit/fileanddabase/util/TextFileManager.java
@@ -0,0 +1,19 @@
+package kr.co.tjeit.fileanddabase.util;
+
+import android.content.Context;
+
+/**
+ * Created by user on 2017-09-11.
+ */
+
+public class TextFileManager {
+
+    private final static String memoFileName = "memo.txt";
+
+    Context mContext;
+
+    public TextFileManager(Context context) {
+        mContext = context;
+    }
+
+}
